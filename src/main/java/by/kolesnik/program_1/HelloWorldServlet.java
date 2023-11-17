3af88ee5ef1b2ee9e29bfbe4297d95d294add4c4
mkole <margaritakolesnik2003@gmail.com>
2023-11-17 16:09:19 +0300

    should be in branch

---
+++ src/main/java/by/kolesnik/program_1/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package by.kolesnik.program_1;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class HelloWorldServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ZoneId zoneId = ZoneId.of(req.getParameter("zone") == null ? "Europe/Minsk" : req.getParameter("zone"));
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
+        resp.getWriter().println(zonedDateTime.getHour()
+                + ":" + zonedDateTime.getMinute() + ":" + zonedDateTime.getSecond());
+    }
+}
